df716dc2dfe1e9918fd328ea7fcf75018f5e64ad
samarth <67502334+s4marth@users.noreply.github.com>
2021-07-05 12:34:08 +0530

    Create secant.cpp

---
+++ secant.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+#include<cmath>
+using namespace std;
+
+float f(float x)
+{
+    return((x*x)-25);
+}
+int main()
+{
+    int i;
+    float a,b,m,e=0.0001;
+    cout<<"enter the values of a and b";
+    cin>>a>>b;
+
+    m=(a*f(b)-b*f(a))/(f(b)-f(a));
+    i=1;
+    while(fabs(f(m))>=e)
+    {
+        cout<<"\n"<<i<<"\t"<<a<<"\t"<<b<<"\t"<<m<<"\t"<<f(m)<<"\t"<<f(a)*f(m);
+        a=m;
+        b=m;
+        m=(a*f(b)-b*f(a))/(f(b)-f(a));
+        i=i+1;
+    }
+    cout<<"\n"<<i<<"\t"<<a<<"\t"<<b<<"\t"<<m<<"\t"<<f(m)<<"\t"<<f(a)*f(m);
+    cout<<"\n root="<<m;
+
+    return 0;
+}
